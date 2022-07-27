c885e3b405920bed083312c25699aff36b315f83
Verena Jasmin Hallitschke <verena.hallitschke@inovex.de>
2022-07-27 08:39:02 +0000

    New refactoring

--- src/fl_server/flwr_server.py
+++ src/fl_server/flwr_server.py
@@ -1,28 +1,30 @@
 """
-This scripts starte a flower server using a customized flower training strategy
+This script starts a flower server using a customized flower training strategy
 """
 from typing import List
 
 import flwr as fl
 import mlflow
+from fl_models.util.dynamic_loader import load_usecase
 from fl_server.flwr_server_fed_avg import FedAvgPlus
 
 
-# Start Flower server for ten rounds of federated learning
 # pylint: disable= too-many-arguments
 def start_server(
     num_clients: int,
-    test_bearings: List[str],
+    name: str,
     num_fed_rounds: int,
     flwr_server_address: str = "localhost:8080",
     ml_flow_server_address: str = "./mlruns",
     ml_flow_run=None,
+    **kwargs
 ):
     """
-
+    Starts a flower server for the given usecase. The remaining keyword arguments are used as \
+    arguments for the usecase instantiation and are usecase-dependant
     :param num_clients: Number of clients necessary to start training
-    :param test_bearings: List of bearing names to evaluate global model on
-    :param num_fed_rounds: Number of federaed training rounds
+    :param name: Name of the usecase that should be loaded
+    :param num_fed_rounds: Number of federated training rounds
     :param flwr_server_address: The address this server is hosted at
     :param ml_flow_server_address: The mlflow tracking URI either a folder path or a network address
     :param ml_flow_run: mlflow run ID the client will save the logging to.
@@ -37,13 +39,16 @@ def start_server(
     if ml_flow_run is not None:
         mlflow.start_run(run_id=ml_flow_run, nested=True)
 
-    mlflow.log_param("test_bearings", test_bearings)
+    # mlflow.log_param("test_bearings", test_bearings)
     mlflow.log_param("number_clients", num_clients)
     mlflow.log_param("number_of_federated_trainingrounds", num_fed_rounds)
 
+    # Load usecase
+    usecase = load_usecase(name, **kwargs)
+
     # Create strategy
     strategy = FedAvgPlus(
-        test_bearings,
+        usecase,
         fraction_fit=1.0,
         fraction_eval=1.0,
         min_fit_clients=num_clients,
@@ -57,7 +62,7 @@ def start_server(
 
     stringlist = []
     # pylint: disable= unnecessary-lambda
-    strategy.degradation_model.prediction_model.summary(
+    usecase.get_model().prediction_model.summary(
         print_fn=lambda x: stringlist.append(x)
     )
     mlflow.log_param("model_architecture", stringlist)
@@ -82,4 +87,9 @@ if __name__ == "__main__":
         "Bearing2_7",
         "Bearing3_3",
     ]
-    start_server(num_clients=3, test_bearings=default_test_bearings, num_fed_rounds=10)
+    start_server(
+        num_clients=3,
+        name="Test",
+        test_bearings=default_test_bearings,
+        num_fed_rounds=10,
+    )
--- src/fl_server/util/constants.py
+++ src/fl_server/util/constants.py
@@ -1,8 +1,13 @@
 """
 Provides constants
 """
+
+import os
+
 # Logging Paths
-LOG_PATH = "../../logs"
+LOG_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "logs"
+)
 LOG_FILES_PATH = LOG_PATH + "/files"
 METRICS_DICT_PATH = LOG_PATH + "/metrics_dict"
 LOG_PICTURES_PATH = LOG_PATH + "/pictures"
